9334377d4042a008203d9d198ba4f89d488f71dd
Andrew Sheley <andrew.sheley@gmail.com>
2020-01-25 01:52:45 -0600

    Add new method.

--- Demo/Helpers/Utils.cs
+++ Demo/Helpers/Utils.cs
@@ -8,5 +8,7 @@ namespace Demo.Helpers
     public static class Utils
     {
         public static string DisplayName(string firstName, string lastName) => $"{firstName}, {lastName}";
+
+        public static int MultiplyMe(int first, int second) => first * second;
     }
 }
